f4aa8c167394d7419531e7b90cfbb8057ad2ccb4
Dylan <dylan.reeder5@gmail.com>
2023-04-13 10:42:08 -0400

    filter user and populate feed

--- server/src/routers/users.ts
+++ server/src/routers/users.ts
@@ -4,8 +4,16 @@ import { t } from '../trpc';
 import { PrismaClient } from '@prisma/client';
 import { decode } from 'jsonwebtoken';
 import { JWT } from '../types/JWT';
+import { findNearUsers } from '../utils/findNearbyUsers';
+import { filterFeedHelper } from '../utils/filterFeedHelper';
+import { FilterType } from '../../../client/utils/types/filter';
 
 const prisma = new PrismaClient();
+
+export type Filter = {
+  filter: string;
+  value: string | boolean;
+};
 
 export const usersRouter = t.router({
   all: t.procedure.query(async () => {
@@ -100,6 +108,7 @@ export const usersRouter = t.router({
 
             location: {
               create: {
+                radius: 5,
                 lat: input.gym.longitude,
                 long: input.gym.latitude,
               },
@@ -130,6 +139,7 @@ export const usersRouter = t.router({
         },
         data: {
           authSteps: input.authSteps,
+
           gym: {
             connect: {
               id: gym.id,
@@ -240,6 +250,7 @@ export const usersRouter = t.router({
       })
     )
     .mutation(async ({ input }) => {
+      console.log(input);
       const decoded = decode(input.token as string) as JWT;
 
       try {
@@ -265,11 +276,156 @@ export const usersRouter = t.router({
             tags: {
               set: input.tags || user.tags,
             },
-            authSteps: input.authSteps || user.authSteps,
+            authSteps:
+              input.authSteps !== undefined ? input.authSteps : user.authSteps,
           },
         });
 
         return updatedUser;
+      } catch (error) {
+        console.log(error);
+        throw new Error('error');
+      }
+    }),
+
+  getNearByUsers: t.procedure
+    .input(z.object({ token: z.string() }))
+    .mutation(async ({ input }) => {
+      const decoded = decode(input.token as string) as JWT;
+
+      try {
+        const user = await prisma.user.findUnique({
+          where: {
+            email: decoded.email,
+          },
+          include: {
+            chats: {
+              select: {
+                user: {
+                  select: {
+                    id: true,
+                  },
+                },
+              },
+            },
+            feed: true,
+          },
+        });
+
+        if (!user) {
+          console.log('User not found');
+          throw new Error('User not found');
+        }
+        const users = await findNearUsers(user);
+
+        // remove users that are already in the user's chat list
+        let usersToAdd = users.filter(
+          (u) => !user.chats.some((c) => c.user?.id === u.id)
+        );
+
+        const userGenderFilers = user.filterGender as string[];
+        const userGoalFilters = user.filterGoals as string[];
+        const userSkillLevelFilters = user.filterSkillLevel as string[];
+        const userWorkoutFilters = user.filterWorkout as string[];
+        const userGoingTodayFilters = user.filterGoingToday as boolean;
+
+        if (
+          userGenderFilers.length > 0 ||
+          userGoalFilters.length > 0 ||
+          userSkillLevelFilters.length > 0 ||
+          userWorkoutFilters.length > 0 ||
+          userGoingTodayFilters === true ||
+          userGoingTodayFilters === false
+        ) {
+          usersToAdd = filterFeedHelper(usersToAdd, user);
+        }
+        await prisma.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            feed: {
+              connect: usersToAdd.map((user) => ({
+                id: user.id,
+              })),
+            },
+          },
+          include: {
+            feed: true,
+            split: true,
+          },
+        });
+
+        return usersToAdd;
+      } catch (error) {
+        console.log(error);
+        throw new Error('error');
+      }
+    }),
+
+  filterFeed: t.procedure
+    .input(
+      z.object({
+        token: z.string(),
+        // filters: Filter[]
+        filters: z.array(
+          z.object({
+            filter: z.string(),
+            value: z.array(z.string()).or(z.boolean()),
+          })
+        ),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const decoded = decode(input.token as string) as JWT;
+      console.log('htting', input.filters);
+
+      try {
+        const user = await prisma.user.findUnique({
+          where: {
+            email: decoded.email,
+          },
+          include: {
+            feed: true,
+          },
+        });
+
+        if (!user) {
+          console.log('User not found');
+          throw new Error('User not found');
+        }
+
+        const findFilterValue = (filterName: FilterType) => {
+          return input.filters.find((filter) => filter.filter === filterName)
+            ?.value as string[] | boolean;
+        };
+
+        // apply filters to user
+        const userWithFilters = await prisma.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            // @ts-expect-error - it is a boolean in an array
+            filterGoingToday: findFilterValue(
+              FilterType.GOING_TODAY
+            )?.[0] as boolean,
+            filterWorkout: findFilterValue(FilterType.WORKOUT_TYPE) as string[],
+            filterSkillLevel: findFilterValue(
+              FilterType.SKILL_LEVEL
+            ) as string[],
+            filterGender: findFilterValue(FilterType.GENDER) as string[],
+            filterGoals: findFilterValue(FilterType.GOALS) as string[],
+          },
+        });
+
+        const filteredFeed = filterFeedHelper(user.feed, userWithFilters);
+
+        console.log(filteredFeed.map((user) => user.firstName));
+
+        return {
+          feed: filteredFeed.filter((user) => user.id !== userWithFilters.id),
+        };
       } catch (error) {
         console.log(error);
         throw new Error('error');
